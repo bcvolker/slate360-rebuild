b83f591cfb03d11b8fd279118ea9a093b34b452e
bcvolker <slate360ceo@gmail.com>
2025-09-21 01:25:04 +0000

    Fix: Remove tiles prop from HomePageClient to match restored design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
+
 import HomePageClient from './PageClient';
-import { tileData } from '@/lib/tileData';
 
 export default function Page() {
-  return <HomePageClient tiles={tileData} />;
+  return <HomePageClient />;
 }
