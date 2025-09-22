9837fd178a56a78152dec7e2b13bfa3ee49c4e09
bcvolker <slate360ceo@gmail.com>
2025-09-22 01:29:57 +0000

    fix: Update ClientHeader to match new Header signature (no activeSection prop)

--- components/ui/ClientHeader.tsx
+++ components/ui/ClientHeader.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import Header from "./Header";
 
-type HeaderProps = { activeSection: string | null };
-
-export default function ClientHeader({ activeSection }: HeaderProps) {
-  return <Header activeSection={activeSection} />;
+export default function ClientHeader() {
+  return <Header />;
 }
