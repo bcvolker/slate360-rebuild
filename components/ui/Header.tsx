ccb00ff03f56067df537283099762d013c438ac4
bcvolker <slate360ceo@gmail.com>
2025-09-23 05:15:05 +0000

    test(header): Use test-logo.png to diagnose static asset path issues

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <header style={{ width: '100%', background: '#fff', padding: '16px 0', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: '1px solid #eee' }}>
       <img
-        src="/slate360-logo.png"
-        alt="Slate360 Logo"
+        src="/test-logo.png"
+        alt="Test Logo"
         style={{ height: '80px', width: 'auto', objectFit: 'contain', display: 'block' }}
       />
     </header>
