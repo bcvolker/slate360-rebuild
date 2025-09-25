5521a886d98d2dc0def15347c930c60bc10ae190
bcvolker <slate360ceo@gmail.com>
2025-09-25 22:16:06 +0000

    Step 0: Cleanup legacy header/zombie code

--- components/ui/Section.tsx
+++ components/ui/Section.tsx
@@ -10,7 +10,7 @@ export default function Section({
       id={id}
       className={clsx(
         'snap-start min-h-[90vh] py-12 md:py-20 flex items-center',
-        'scroll-mt-[88px]', // for blue header + tile nav
+        'scroll-mt-[64px]', // for slim navbar
         className
       )}
     >
