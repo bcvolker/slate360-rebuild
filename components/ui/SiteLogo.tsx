0d27247bc8e61d1b5216611af2b3650fcd38edfc
bcvolker <slate360ceo@gmail.com>
2025-09-25 06:54:40 +0000

    feat: decouple SiteLogo from navbar

--- components/ui/SiteLogo.tsx
+++ components/ui/SiteLogo.tsx
@@ -3,21 +3,15 @@ import Link from 'next/link';
 
 export default function SiteLogo() {
   return (
-    <Link
-      href="/"
-      aria-label="Go to Homepage"
-      className="fixed left-6 top-[52px] z-50 block"
-    >
-      <div className="relative h-[74px] w-[265px]">
-        <Image
-          src="/slate360logoforwebsite.png"
-          alt="Slate360 Logo"
-          fill
-          priority
-          sizes="265px"
-          className="object-contain drop-shadow-md"
-        />
-      </div>
+    <Link href="/" className="fixed top-1 left-6 z-50 pointer-events-auto">
+      <Image
+        src="/slate360logoforwebsite.png"
+        alt="Slate360 Logo"
+        width={220}
+        height={74}
+        priority
+        className="h-auto w-auto object-contain"
+      />
     </Link>
   );
 }
