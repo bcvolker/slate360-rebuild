c9f09ecae59fc20e880dde688df1b35a5c357d41
bcvolker <slate360ceo@gmail.com>
2025-09-30 06:24:47 +0000

    Revert "Enhance mobile scrolling responsiveness"
    This reverts commit 36023c6bf81093ec7a7b6ace7c6e97ecc29f3bc2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   <body className={`bg-white text-gray-900 overflow-x-hidden md:overflow-hidden ${inter.className}`}>
     <Navbar />
     <DebugScrollOverlay />
-  <main id="scroll-container" className="h-auto md:h-[calc(100vh-5rem)] overflow-visible md:overflow-y-scroll pt-20 bg-white md:overscroll-contain touch-pan-y">{children}</main>
+  <main id="scroll-container" className="h-auto md:h-[calc(100vh-5rem)] overflow-visible md:overflow-y-scroll pt-20 bg-white overscroll-contain touch-pan-y">{children}</main>
       </body>
     </html>
   );
