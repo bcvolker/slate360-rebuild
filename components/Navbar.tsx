2c9a28b87d32d713e39b095e70baa700fc4e647a
bcvolker <slate360ceo@gmail.com>
2025-11-12 08:44:31 +0000

    final: clean up + ESLint

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,5 @@
-export { default } from "./NavbarFixed";
+// The real Navbar implementation lives in NavbarFixed for now.
+// This file re-exports it so other imports use `components/Navbar`.
+import NavbarFixed from "./NavbarFixed";
+
+export default NavbarFixed;
